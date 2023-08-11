6c093f620af588ef580c6c7212e5fad0699d7a94
GubbyDuo <57397396+GubbyDuo@users.noreply.github.com>
2023-08-11 10:34:55 +1000

    initial commit

---
+++ src/test/java/com/revenology/travelapp/TravelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revenology.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ travelapp/src/main/java/com/revenology/travelapp/TravelappApplication.java
@@ -0,0 +1,13 @@
+package com.revenology.travelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelappApplication.class, args);
+	}
+
+}
