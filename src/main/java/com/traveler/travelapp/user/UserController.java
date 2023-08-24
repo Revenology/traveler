d38cc94af63aa988b0808d5a7a89d9f003035204
jpchaib <joaopedrochaib@gmail.com>
2023-08-25 08:59:35 +1000

    Set the database in application.properties, add @RequestMapping in UserController, add MySQL dependency in pom file.

--- src/main/java/com/traveler/travelapp/user/UserController.java
+++ src/main/java/com/traveler/travelapp/user/UserController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/users")
 public class UserController {
     
     private final UserRepository userRepository;
