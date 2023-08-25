7ad164cd152515feaf051abe78bd0c89164e6610
GubbyDuo <57397396+GubbyDuo@users.noreply.github.com>
2023-08-25 14:57:32 +1000

    Added Lombok Features

--- src/main/java/com/traveler/travelapp/loginRequest/LoginRequest.java
+++ src/main/java/com/traveler/travelapp/loginRequest/LoginRequest.java
@@ -1,27 +1,16 @@
 package com.traveler.travelapp.loginRequest;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor 
 public class LoginRequest {
     private String userName;
     private String password;
 
-    public LoginRequest(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/traveler/travelapp/user/User.java
+++ src/main/java/com/traveler/travelapp/user/User.java
@@ -3,45 +3,19 @@ package com.traveler.travelapp.user;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private @Id @GeneratedValue Long id;
     private String name;
     private String userName;
     private String password;
-
-    //Constructors
-    public User (){}
-    
-    public User(String name, String userName, String password) {
-        this.name = name;
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public User(String userName, String password){
-        this.userName = userName;
-        this.password = password;
-    }
-
-    //Setters and getters
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String passWord) {
-        this.password = passWord;
-    }
 }
